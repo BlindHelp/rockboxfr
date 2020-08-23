0bc8c927ae97d63f61028a555598382a02c4052f
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-08-23 19:22:33 +0200

    Fix script

--- scripts/footer.js
+++ scripts/footer.js
@@ -4,5 +4,5 @@ var update = new Date(document.lastModified);
 var day = update.getDate();
 var month = update.getMonth();
 var year = update.getFullYear();
-div.innerHTML = "<p>Última actualización: <time>" + day + "/" + month + 1 + "/" + year + "</time></p>";
+div.innerHTML = "<p>Dermier mise: <time>" + day + "/" + (month + 1) + "/" + year + "</time></p>";
 footer.appendChild(div);
