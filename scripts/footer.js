eec84b97b797386685e009ac99401aac92c96ac1
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-08-23 19:14:44 +0200

    Try to fix script

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,13 +1,8 @@
-var months = ["enero", "febrero", "marzo",
-"abril", "mayo", "junio",
-"julio", "agosto", "septiembre",
-"octubre", "noviembre", "diciembre"];
-
 var footer = document.getElementById("footer");
 var div = document.createElement("DIV");
 var update = new Date(document.lastModified);
 var day = update.getDate();
 var month = update.getMonth();
 var year = update.getFullYear();
-div.innerHTML = "<p>Última actualización: <time>" + day + " de " + months[month] + " de " + year + "</time></p>"
+div.innerHTML = "<p>Última actualización: <time>" + day + "/" + month + 1 + "/" + year + "</time></p>";
 footer.appendChild(div);
