5f66c298bf8c14c538785920eac1b31b57037a0a
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-08-23 19:03:58 +0200

    Add script for testing

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,8 +1,13 @@
+var months = ["enero", "febrero", "marzo",
+"abril", "mayo", "junio",
+"julio", "agosto", "septiembre",
+"octubre", "noviembre", "diciembre"];
+
 var footer = document.getElementById("footer");
 var div = document.createElement("DIV");
 var update = new Date(document.lastModified);
 var day = update.getDate();
 var month = update.getMonth();
 var year = update.getFullYear();
-div.innerHTML = "<p>Dernière mise: <time>" + day + "/" + month + "/" + year + "</time></p>"
+div.innerHTML = "<p>Última actualización: <time>" + day + " de " + months[month] + " de " + year + "</time></p>"
 footer.appendChild(div);
