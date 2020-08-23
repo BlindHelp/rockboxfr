54148d5f0eb7be7bd8a9e51f4eeb65c6bd1da1e7
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-08-23 18:45:48 +0200

    Fix for script

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,8 +1,8 @@
 var footer = document.getElementById("footer");
-var div = document.createElement("div");
+var div = document.createElement("DIV");
 var update = new Date(document.lastModified);
 var day = update.getDate();
 var month = update.getMonth();
 var year = update.getFullYear();
-div.innerHTML = "<p>Dernier: <time>" + day + "/" + month + "/" + year + "</time></p>"
+div.innerHTML = "<p>Dernière mise: <time>" + day + "/" + month + "/" + year + "</time></p>"
 footer.appendChild(div);
