6dd8e953530a5c5751fe6d623e3551577e68b3e0
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-08-24 08:47:24 +0200

    Fix class

--- scripts/footer.js
+++ scripts/footer.js
@@ -3,6 +3,7 @@ import ConvertDate from './convertDate.js';
 var footer = document.getElementById("footer");
 var div = document.createElement("DIV");
 var update = new Date(document.lastModified);
+var convertDocumentDate = new ConvertDate(update);
 
-div.innerHTML = "<p>Dernière mise à jour: <time>" + ConvertDate(update).convertedDate + "</time></p>";
+div.innerHTML = "<p>Dernière mise à jour: <time>" + convertDocumentDate.convertedDate + "</time></p>";
 footer.appendChild(div);
