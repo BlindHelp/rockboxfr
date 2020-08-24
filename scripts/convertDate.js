88ed9c2a57035fc5af5440249d60ce0911e69d6b
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-08-24 08:54:25 +0200

    Fix

--- scripts/convertDate.js
+++ scripts/convertDate.js
@@ -2,7 +2,7 @@ export default class ConvertDate {
 
 		constructor(originalDate) {
 			this.originalDate = originalDate;
-		this.day = this.date.getDate();
+		this.day = this.originalDate.getDate();
 		if (this.day < 10) { this.day = "0" + this.day };
 		this.month = this.originalDate.getMonth() + 1;
 		if (this.month < 10) { this.month = "0" + this.month };
